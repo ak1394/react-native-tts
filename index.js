5443048ca90c93bec69764d3ee42bfd63a94f81c
Anton Krasovsky <anton.krasovsky@gmail.com>
2016-09-28 23:09:55 +0100

    Fix lang parameter passing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { NativeModules, NativeAppEventEmitter } from 'react-native';
 
 export function speak(text, lang) {
-  return NativeModules.TextToSpeech.speak({text: text, lang: lang});
+  return NativeModules.TextToSpeech.speak({text: text, language: lang});
 }
 
 export function voices() {
